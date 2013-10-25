7cb33b19d5e4a6c76e1022240c592c7520ced4b7
Mickey Kay <mickey@mickeykaycreative.com>
2013-10-25 12:48:46 -0700

    v. 1.4 - fixed nav extras CSS

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis - Mobile First' );
 define( 'CHILD_THEME_URL', 'http://www.mightyminnow.com/2013/08/our-new-mobile-first-child-theme-for-genesis-2-0/' );
-define( 'CHILD_THEME_VERSION', '1.3' );
+define( 'CHILD_THEME_VERSION', '1.4' );
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5' );
